baf1641b269c83e5dd96a1ad4f324f5c305faae7
Duvan <duvan4448@gmail.com>
2020-05-07 22:21:36 -0500

    1-Constantes

---
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello\tWorld \n Hola Mundo");
+            //Console.ReadKey();
+
+            const double pi = 3.14159;
+
+
+            double radio;
+
+            Console.WriteLine("Introduzca el valor del radio:");
+            radio = Convert.ToDouble(Console.ReadLine());
+
+            double areaCirculo = pi * radio * radio;
+
+            Console.WriteLine("El radio es: {0}, y el Área del circulo es: {1}", radio, areaCirculo);
+
+            Console.ReadKey();
+        }
+    }
+}
