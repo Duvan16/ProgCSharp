92b4ab721a6d9410f064602a6d20b43edae88564
Duvan <duvan4448@gmail.com>
2020-05-07 23:19:49 -0500

    10-ManejoExcepciones

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -7,42 +7,35 @@ using System.Threading.Tasks;
 
 namespace ProgCSharp
 {
-    class Program
+    class DivNumeros
     {
+        int resultado;
+
+        DivNumeros()
+        {
+            resultado = 0;
+        }
+
+        public void Division(int num1, int num2)
+        {
+            try
+            {
+                resultado = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Se ha producido una excepción del tipo: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("EL resultado final es: {0}", resultado);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Queue q = new Queue();
-
-            q.Enqueue(1);
-            q.Enqueue(2);
-            q.Enqueue(3);
-
-            Console.WriteLine("Los Elementos de la lista son:");
-
-            while (q.Count > 0)
-            {
-                Console.WriteLine(q.Dequeue());
-            }
-
-            Console.WriteLine("Ahora el conteinido de nuestra cola son:" + q.Count);
-
-
-            //Implementación stack
-
-            Stack s = new Stack();
-
-            s.Push(1);
-            s.Push(2);
-            s.Push(3);
-
-            Console.WriteLine("Los Elementos que tenemos en la pila son:");
-
-            while (s.Count > 0)
-            {
-                Console.WriteLine(s.Pop());
-            }
-
-            Console.WriteLine("Los Elementos que hay ahora en la pila son:" + s.Count);
+            DivNumeros d = new DivNumeros();
+            d.Division(23, 2);
             Console.ReadKey();
         }
     }
