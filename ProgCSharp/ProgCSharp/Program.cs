c67759219a17692b55d8a7c8c35f6d09a5e8392b
Duvan <duvan4448@gmail.com>
2020-05-07 22:33:20 -0500

    3-Strings

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -10,14 +10,64 @@ namespace ProgCSharp
     {
         static void Main(string[] args)
         {
-            string[] pilotos = new string[4] { "Alonso", "Hamilton", "Verstappen", "Sainz" };
-            Console.WriteLine(pilotos[2]);
-            Console.WriteLine();
-            pilotos[2] = "Vettel";
-            foreach (string piloto in pilotos)
+            // método string
+            string nombre = "Alexander Gonzalez";
+            string apellidos = nombre.Substring(10, 8);
+
+            Console.WriteLine(apellidos);
+
+
+            // método indexOf
+            string nombre1 = "Alexandre Gonzalez - Programando en c#";
+            int posicion1 = nombre1.IndexOf("Programando");
+            Console.WriteLine(posicion1);
+
+            int posicion2 = nombre1.IndexOf("Gonzalez", 5);
+            Console.WriteLine(posicion2);
+
+
+            // funciones startwith y Endwith
+            string archivo = "comc123.dll";
+
+            if (archivo.StartsWith("com"))
             {
-                Console.WriteLine(piloto);
+                Console.WriteLine("El archivo comienza con el texto COM");
             }
+
+            if (archivo.EndsWith(".dll"))
+            {
+                Console.WriteLine("El archivo es una libreria");
+            }
+
+
+            //Extensiones PadLeft y PadRight
+
+            string nombre4 = "Gonzalez";
+            string codigo = "123";
+
+            nombre4 = nombre4.PadRight(10, 'X');
+            codigo = codigo.PadLeft(6, '0');
+
+            Console.WriteLine(nombre4 + codigo);
+
+
+            // funciones Split y Join
+
+            string linea = "Prueba, 10 , 20, 02/05/2020";
+            string[] campos = linea.Split(new char[] { ',' });
+            Console.WriteLine(campos[0]);
+            Console.WriteLine(campos[1]);
+            Console.WriteLine(campos[2]);
+            Console.WriteLine(campos[3]);
+
+
+            string lineanueva = string.Join(";", campos);
+            Console.WriteLine(lineanueva);
+
+
+            // función format
+            string formato = string.Format("Aquí voy a poner el {0}. Ahora son las {1}. Este es un valor numerico {2}", "parámetro que yo quiera poner", DateTime.Now, 15);
+            Console.WriteLine(formato);
             Console.ReadKey();
         }
     }
