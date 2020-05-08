fbb4b47eb100264f9399c52e04c183e469513354
Duvan <duvan4448@gmail.com>
2020-05-08 00:57:33 -0500

    14-Generics-Rendimiento

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -9,32 +9,13 @@ namespace ProgCSharp
 {
     class Program
     {
-        public class usuario
-        {
-            int id { get; set; }
-            string nombre { get; set; }
-        }
         static void Main(string[] args)
         {
-            //Colección NO Genérica ArrayList
-            //ArrayList objetos = new ArrayList();
+            ArrayList objetos = new ArrayList();
 
-            //objetos.Add(1);
-            //objetos.Add("Angel Arias");
-            //objetos.Add(new usuario());
+            objetos.Add(50);//boxing
 
-            //foreach (int i in objetos)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            //Console.ReadKey();
-
-            //Colección Genérica List<T>
-            List<int> objetos = new List<int>();
-
-            objetos.Add(1);
-            objetos.Add(2);
-            objetos.Add(3);
+            int x = (int)objetos[0]; //unboxing
 
             foreach (int i in objetos)
             {
