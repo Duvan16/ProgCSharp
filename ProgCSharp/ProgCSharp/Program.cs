c60bc2dd7398f5f63fadf7d934e5d0774354f134
Duvan <duvan4448@gmail.com>
2020-05-08 01:09:00 -0500

    15-Generics-Reutilizacion

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -7,21 +7,44 @@ using System.Threading.Tasks;
 
 namespace ProgCSharp
 {
-    class Program
+    public class usuario
     {
+        public int id { get; set; }
+        public string nombre { get; set; }
+    }
+    public class ListaElementos<T>
+    {
+        private List<T> elementos;
+
+        public ListaElementos()
+        {
+            elementos = new List<T>();
+        }
+
+        public void Agregar(T nuevoelemento)
+        {
+            elementos.Add(nuevoelemento);
+        }
+
+        public void Eliminar(T eliminarelemento)
+        {
+            elementos.Remove(eliminarelemento);
+        }
+
         static void Main(string[] args)
         {
-            ArrayList objetos = new ArrayList();
+            ListaElementos<int> Entero = new ListaElementos<int>();
+            Entero.Agregar(1);
+            Entero.Eliminar(1);
 
-            objetos.Add(50);//boxing
+            ListaElementos<string> cadenas = new ListaElementos<string>();
+            cadenas.Agregar("Duvan Gonzalez");
+            cadenas.Eliminar("Duvan Gonzalez");
 
-            int x = (int)objetos[0]; //unboxing
+            ListaElementos<usuario> Objetos = new ListaElementos<usuario>();
+            Objetos.Agregar(new usuario { id = 1, nombre = "Duvan Gonzalez" });
+            Objetos.Eliminar(new usuario { id = 1, nombre = "Duvan Gonzalez" });
 
-            foreach (int i in objetos)
-            {
-                Console.WriteLine(i);
-            }
-            Console.ReadKey();
         }
     }
 }
