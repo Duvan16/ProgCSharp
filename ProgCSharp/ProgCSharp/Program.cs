24d7edf7595e5e26e87054a8c736d249a256d36b
Duvan <duvan4448@gmail.com>
2020-05-10 10:27:44 -0500

    20-Delegados

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -2,57 +2,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+
+delegate int numeros(int n);
 
 namespace ProgCSharp
 {
     class Program
     {
-        public interface IOrganismo
-        {
-            void respirar();
-            void mover();
+        static int numero = 10;
+
+        public static int sumarNumeros(int p) {
+            numero += p;
+            return numero;
         }
 
-        public interface IAnimal
+        public static  int multiplicarNumero(int p)
         {
-            void mover();
-            void viviparo();
+            numero *= p;
+            return numero;
         }
 
-        class Perro : IOrganismo, IAnimal
+        public static int obtenerNumero()
         {
-            public void respirar()
-            {
-                Console.Write("Este Organismo respira");
-            }
-            void IOrganismo.mover()
-            {
-                Console.WriteLine("Este Organismo se mueve");
-            }
-            void IAnimal.mover()
-            {
-                Console.WriteLine("Este Animal se mueve");
-            }
-
-            public void viviparo()
-            {
-                Console.WriteLine("Este Organismo es viviparo");
-            }
+            return numero;
         }
 
         static void Main(string[] args)
         {
-            Perro perros = new Perro();
-            IOrganismo organismo = perros;
-            IAnimal animal = perros;
+            numeros num;
+            numeros numero1 = new numeros(sumarNumeros);
+            numeros numero2 = new numeros(multiplicarNumero);
 
-            perros.respirar();
-            perros.viviparo();
+            num = numero1;
+            num += numero2;
 
-            organismo.mover();
-            animal.mover();
+            num(5);
+
+            Console.WriteLine("El valor de la variable num es: {0}", obtenerNumero());
+
+            /*numero1(10);
+            Console.WriteLine("El valor de la instancia numero1 es: {0}", obtenerNumero());
+
+            numero2(5);
+            Console.WriteLine("El valor de la instancia numero2 es {0}", obtenerNumero());*/
 
             Console.ReadKey();
         }
