353d49e34a8bf866028ea64d88ff6c6aca107a53
Duvan <duvan4448@gmail.com>
2020-05-13 23:14:25 -0500

    28-Directorios

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -33,15 +33,78 @@ namespace ProgCSharp
             //Console.ReadKey();
 
             // Eliminar Ficheros
-            if (File.Exists("prueba.txt"))
+            //if (File.Exists("prueba.txt"))
+            //{
+            //    File.Delete("prueba.txt");
+            //    if (File.Exists("prueba.txt") == false)
+            //        Console.WriteLine("El archivo se ha eliminado...");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("El archivo buscado ya no existe");
+            //}
+
+            //ELiminar Directorio
+            //if (Directory.Exists("dirprueba"))
+            //{
+            //    Directory.Delete("dirprueba", true);
+            //    if (Directory.Exists("dirprueba") == false)
+            //        Console.WriteLine("Se ha eliminado el directorio...");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("El Directorio buscado ya no existe");
+            //}
+
+            //Renombrar archivos
+            //if (File.Exists("prueba.txt"))
+            //{
+            //    Console.WriteLine("Introduce el nuevo nombre del archivo");
+
+            //    string nuevoArchivo = Console.ReadLine();
+
+            //    if (nuevoArchivo != string.Empty)
+            //    {
+            //        File.Move("prueba.txt", nuevoArchivo);
+            //        if (File.Exists(nuevoArchivo))
+            //        {
+            //            Console.WriteLine("El nombre del nuevo archivo es:" + nuevoArchivo);
+            //        }
+            //    }
+            //}
+
+            //Renombrar Directorio
+            //if (Directory.Exists("dirprueba"))
+            //{
+            //    Console.WriteLine("Introduce el nuevo nombre del Directorio");
+
+            //    string nuevoDirectorio = Console.ReadLine();
+
+            //    if (nuevoDirectorio != string.Empty)
+            //    {
+            //        Directory.Move("dirprueba", nuevoDirectorio);
+
+            //        if (Directory.Exists(nuevoDirectorio))
+            //            Console.WriteLine("EL nuevo Directorio se llama: " + nuevoDirectorio);
+            //    }
+            //}
+            //Console.ReadKey();
+
+            // Crear un Directorio
+
+            Console.WriteLine("Introduce el nombre del Directorio");
+
+            string crearDirectorio = Console.ReadLine();
+
+            if (crearDirectorio != string.Empty)
             {
-                File.Delete("prueba.txt");
-                if (File.Exists("prueba.txt") == false)
-                    Console.WriteLine("El archivo se ha eliminado...");
-            }
-            else
-            {
-                Console.WriteLine("El archivo buscado ya no existe");
+                Directory.CreateDirectory(crearDirectorio);
+
+                if (Directory.Exists(crearDirectorio))
+                {
+                    Console.WriteLine("EL nombre del Directorio que hemos creado es:" + crearDirectorio);
+
+                }
             }
             Console.ReadKey();
         }
