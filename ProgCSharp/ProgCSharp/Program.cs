8517fbbf6d66d727fd84cd567624dc35ff01f1fa
Duvan <duvan4448@gmail.com>
2020-05-16 00:26:01 -0500

    34-EjemplosLambda

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -14,33 +14,48 @@ namespace ProgCSharp
 {
     class Program
     {
-        //delegate int delegado(int i);
         static void Main(string[] args)
         {
-            //delegado miDelegado = y => y * y;
-            //int j = miDelegado(5);
-            //Console.WriteLine(j);
-            //Console.ReadKey();
+            // DECLARACIONES MULTIPLES DE EXPRESIONES LAMBDA
 
-            /*int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 25, 27, 29, 50 };
+            /*
+            // expresión lambda tipada de manera implicita, creamos una instancia a la expresiónn
+            Func<int, int> expresion1 = x => x + 1;
 
-            double valormedio = numeros.Where(num => num % 2 == 1).Average();
-            Console.WriteLine(valormedio);
-            Console.ReadKey();*/
+            //  usar la expresión lambda en el cuerpo de la declaración
+            Func<int, int> expresion2 = x => { return x + 1; };
 
-            // Árboles de expresión
+            //usamos  parámteros en la expresión lambda
+            Func<int, int> expresion3 = (int x) => x + 1;
 
-            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //usando parámetros en la expresión para usar en el cuerpo de la declaración
+            Func<int, int> expresion4 = (int x) => { return x + 1; };
 
-            foreach (int i in numeros.Where(x => {
-                if (x <= 3)
-                    return true;
-                else if (x >= 7)
-                    return true;
-                return false;
-            }))
+            //usa lambda con varios parámetros
+            Func<int, int, int> expresion5 = (x, y) => x * y;
+
+            // usar lambda en un método delegado
+            Func<int, int> expresion6 = delegate (int x) { return x + 1; };
+
+            // usar lambda en un método delegado sin parámetros
+            Func<int> expresion7 = delegate { return 1 + 1; };
+
+            Console.WriteLine(expresion1.Invoke(1));
+            Console.WriteLine(expresion2.Invoke(1));
+            Console.WriteLine(expresion3.Invoke(1));
+            Console.WriteLine(expresion4.Invoke(1));
+            Console.WriteLine(expresion5.Invoke(1,1));
+            Console.WriteLine(expresion6.Invoke(1));
+            Console.WriteLine(expresion7.Invoke());
+
+            Console.ReadKey();
+            */
+            List<int> numeros = new List<int>() { 1, 2, 3, 4, 6, 7, 8, 9 };
+            List<int> pares = numeros.FindAll(x => (x % 2) == 0);
+
+            foreach (var p in pares)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("Los números pares son: {0}", p);
             }
             Console.ReadKey();
 
