c1e12bcac99753689422f35ff28d9fd3c5600a95
Duvan <duvan4448@gmail.com>
2020-05-17 11:32:32 -0500

    39-TrabajandoConHijlos

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -16,13 +16,56 @@ namespace ProgCSharp
 {
     class Program
     {
+        public static void LLamarHiloHijo()
+        {
+            //Console.WriteLine("El Hilo Hijo se está ejecutando");
+
+            /*
+            Console.WriteLine("Comienza la ejecución del Hilo Hijo");
+
+            int parar = 5000;
+
+            Console.WriteLine("El Hilo Hijo se parará duran {0} segundos", parar / 1000);
+
+            Thread.Sleep(parar);
+
+            Console.WriteLine("El Hilo Hijo continua su ejecución");
+            */
+            try
+            {
+                Console.WriteLine("Comienza la ejecución del Hilo Hijo");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("El Hilo Hijo se ha completado");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("Se ha producido la Excepción: ", e);
+            }
+            finally
+            {
+                Console.WriteLine("No se ha producido ningún error en la ejecución del Hilo Hijo");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Thread hilo = Thread.CurrentThread;
+            ThreadStart refHijo = new ThreadStart(LLamarHiloHijo);
+            Console.WriteLine("El Hilo Principal --> Esta creando el hilo hijo");
 
-            hilo.Name = "Hilo Principal de la Aplicación";
+            Thread HiloHijo = new Thread(refHijo);
+            HiloHijo.Start();
 
-            Console.WriteLine("El Hilo Principal se llama {0}", hilo.Name);
+            Thread.Sleep(2000);
+
+            Console.WriteLine("El Hilo Principal va a destruir el Hilo Hijo");
+
+            HiloHijo.Abort();
+
             Console.ReadKey();
         }
     }
