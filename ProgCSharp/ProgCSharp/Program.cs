328d73fa6adeaa9cb1190b10bdcdf5dec295b838
Duvan <duvan4448@gmail.com>
2020-05-18 23:30:22 -0500

    49-ReflexionTiendas

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -1,24 +1,61 @@
 ﻿using System;
 using System.Reflection;
+using System.Security.Policy;
 
 namespace ProgCSharp
 {
     class Program
     {
+
+        public class Cliente
+        {
+            public string nombre { get; set; }
+
+            public int DNI { get; set; }
+
+            public string   Email { get; set; }
+
+            public int  telefono { get; set; }
+        }
+
+    public static object CombinarClientes(object tiendaA, object tiendaB)
+    {
+        if (tiendaA.GetType()!=tiendaB.GetType())
+        {
+            throw new Exception("Estos parámetros no son del mismo Tipo");
+        }
+
+        Object nuevaTienda = Activator.CreateInstance(tiendaA.GetType());
+
+        foreach (PropertyInfo p in tiendaA.GetType().GetProperties())
+        {
+            Object tiendaAA = tiendaA.GetType().GetProperty(p.Name).GetValue(tiendaA);
+            Object tiendaBB = tiendaB.GetType().GetProperty(p.Name).GetValue(tiendaB);
+            Object tiendaAB = (tiendaAA == null || tiendaAA.ToString() == "" || tiendaAA.ToString() == "0" ? tiendaBB : tiendaAA);
+
+            nuevaTienda.GetType().GetProperty(p.Name).SetValue(nuevaTienda, tiendaAB);
+        }
+        return nuevaTienda;
+    }
+
         static void Main(string[] args)
         {
-            Assembly MiEnsamblaje = Assembly.LoadFile(@"E:\NetCore\ProgCSharp\ProgCSharp\Calculadora\bin\Debug\Calculadora.dll");
-
-            Type MiTipo = MiEnsamblaje.GetType("Calculadora.Class1");
-
-            object instancia = Activator.CreateInstance(MiTipo);
-
-            Type ObjTipo = instancia.GetType();
-
-            foreach (MemberInfo objeto in ObjTipo.GetMembers())
+            Cliente tiendaAAA = new Cliente()
             {
-                Console.WriteLine(objeto.Name);
-            }
+                nombre = "Juan",
+                DNI = 123456,
+                telefono = 1212121
+            };
+
+            Cliente tiendaBBB = new Cliente()
+            {
+                nombre = "Juan Gomez Perez",
+                DNI = 123456,
+                Email = "juan@juan.com"
+            };
+
+            Cliente tiendaABAB = (Cliente)CombinarClientes(tiendaAAA, tiendaBBB);
+            Console.WriteLine("El Cliente {0} tiene el DNI {1}, y también tiene el email {2} y el teléfono {3}", tiendaABAB.nombre, tiendaABAB.DNI, tiendaABAB.Email, tiendaABAB.telefono);
             Console.ReadKey();
         }
     }
