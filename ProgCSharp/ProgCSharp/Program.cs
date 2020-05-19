b865e5aa14258cec25298bdc4c5a89ed1326c979
Duvan <duvan4448@gmail.com>
2020-05-18 23:10:15 -0500

    48-Reflexion

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -1,44 +1,24 @@
 ﻿using System;
-using System.IO;
-using System.Xml.Serialization;
+using System.Reflection;
 
 namespace ProgCSharp
 {
-
-    public class Persona
-    {
-        [XmlElement(Order = 2)]
-        public string nombre;
-        [XmlElement(Order = 1)]
-       // [XmlAttribute("MayorEdad")]
-        public int edad;
-        [XmlIgnore]
-        public string email;
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Persona p = new Persona();
-            p.nombre = "Angel";
-            p.edad = 40;
-            p.email = "juan@gao.com";
+            Assembly MiEnsamblaje = Assembly.LoadFile(@"E:\NetCore\ProgCSharp\ProgCSharp\Calculadora\bin\Debug\Calculadora.dll");
 
-            XmlSerializer xs = new XmlSerializer(typeof(Persona));
+            Type MiTipo = MiEnsamblaje.GetType("Calculadora.Class1");
 
-            using (Stream s = File.Create(@"E:\temp\persona.xml"))
+            object instancia = Activator.CreateInstance(MiTipo);
+
+            Type ObjTipo = instancia.GetType();
+
+            foreach (MemberInfo objeto in ObjTipo.GetMembers())
             {
-                xs.Serialize(s, p);
+                Console.WriteLine(objeto.Name);
             }
-
-            Persona p2 = new Persona();
-
-            using (Stream s = File.OpenRead(@"E:\temp\persona.xml"))
-            {
-                p2 = (Persona)xs.Deserialize(s);
-            }
-
-            Console.WriteLine("El nombre es {0} y tiene {1} años", p2.nombre, p2.edad);
             Console.ReadKey();
         }
     }
