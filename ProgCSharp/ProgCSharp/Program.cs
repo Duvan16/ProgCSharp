876903b7b936df7ea4bf7a3dbc6174f9350c0e67
Duvan <duvan4448@gmail.com>
2020-05-18 22:30:23 -0500

    47-SerializacionXML

--- ProgCSharp/ProgCSharp/Program.cs
+++ ProgCSharp/ProgCSharp/Program.cs
@@ -1,91 +1,44 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
-using System.Text;
-using System.Web.Script.Serialization;
+using System.Xml.Serialization;
 
 namespace ProgCSharp
 {
+
+    public class Persona
+    {
+        [XmlElement(Order = 2)]
+        public string nombre;
+        [XmlElement(Order = 1)]
+       // [XmlAttribute("MayorEdad")]
+        public int edad;
+        [XmlIgnore]
+        public string email;
+    }
     class Program
     {
-        //[DataContract]
-        class Cliente
-        {
-            //[DataMember]
-            public string nombre { get; set; }
-
-            //[DataMember]
-            public string email { get; set; }
-        }
-
         static void Main(string[] args)
         {
-            // clase DataContactJsonSerializer
-            /*
-            Cliente objCliente = new Cliente()
+            Persona p = new Persona();
+            p.nombre = "Angel";
+            p.edad = 40;
+            p.email = "juan@gao.com";
+
+            XmlSerializer xs = new XmlSerializer(typeof(Persona));
+
+            using (Stream s = File.Create(@"E:\temp\persona.xml"))
             {
-                nombre = "Juan",
-                email = "duva@gma.com"
-            };
-
-            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Cliente));
-
-            MemoryStream objMs = new MemoryStream();
-            js.WriteObject(objMs, objCliente);
-            objMs.Position = 0;
-
-            StreamReader sr = new StreamReader(objMs);
-
-            string json = sr.ReadToEnd();
-            objMs.Close();
-
-            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(json)))
-            {
-                DataContractJsonSerializer deserializar = new DataContractJsonSerializer(typeof(Cliente));
-
-                Cliente objCliente2 = (Cliente)deserializar.ReadObject(ms);
-
-                Console.WriteLine(objCliente2.nombre);
-                Console.WriteLine(objCliente2.email);
-                Console.ReadKey();
+                xs.Serialize(s, p);
             }
-            */
 
-            // clase JavaScriptJsonSerializer
+            Persona p2 = new Persona();
 
-            /*Cliente objCliente = new Cliente()
+            using (Stream s = File.OpenRead(@"E:\temp\persona.xml"))
             {
-                nombre = "Juan",
-                email = "duva@gma.com"
-            };
+                p2 = (Persona)xs.Deserialize(s);
+            }
 
-            JavaScriptSerializer js = new JavaScriptSerializer();
-
-            string json = js.Serialize(objCliente);
-
-            JavaScriptSerializer js2 = new JavaScriptSerializer();
-
-            Cliente deserializar = js2.Deserialize<Cliente>(json);
-
-            Console.WriteLine("El nombre {0} tiene el email {1}", deserializar.nombre, deserializar.email);
-            */
-
-            // biblioteca de terceros Json.NET
-
-            Cliente objCliente = new Cliente()
-            {
-                nombre = "Juan",
-                email = "duva@gma.com"
-            };
-
-            string json = JsonConvert.SerializeObject(objCliente);
-
-            Cliente objCliente2 = JsonConvert.DeserializeObject<Cliente>(json);
-
-            Console.WriteLine("El nombre es {0} y tiene el email {1}", objCliente2.nombre, objCliente2.email);
-
+            Console.WriteLine("El nombre es {0} y tiene {1} años", p2.nombre, p2.edad);
             Console.ReadKey();
         }
     }
